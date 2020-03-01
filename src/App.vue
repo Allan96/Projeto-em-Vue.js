00ea07f06c350cc58c8a2eaf6c11e18dad98e873
Allan96 <allan.souza.silva@usp.br>
2020-03-01 16:40:41 -0300

    Ajustando as pastas do projeto

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<Login/>
+</template>
+
+<script>
+import Login from './componentes/login/Login.vue';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+export default {
+   components: {
+    Login
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
